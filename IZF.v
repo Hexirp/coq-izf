009a60bfd238abf4a1c4c90cbf2f14896ad5c209
Hexirp <Hexirp@users.noreply.github.com>
2017-07-02 08:48:38 +0900

    Add IZF axioms

--- IZF.v
+++ IZF.v
@@ -2,3 +2,12 @@
 Axiom SET : Type.
 (* 帰属関係の述語 *)
 Axiom In : SET -> SET -> Prop.
+
+(* 外延性の公理 *)
+Axiom ExtenAx : forall a b, (forall x, iff (In x a) (In x b)) -> a = b.
+(* 空集合の公理 *)
+Axiom EmptyAx : exists e, forall x, not (In x e).
+(* 対の公理 *)
+Axiom PairAx : forall a b, exists c, forall x, iff (In x c) (x = a \/ x = b).
+(* 和集合公理 *)
+Axiom UnionAx : forall a, exists b, forall x, iff (In x b) (exists u, In u a /\ In u x).
