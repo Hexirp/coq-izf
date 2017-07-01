9240e0475ff395000fb90553533755b87f964b78
Hexirp <Hexirp@users.noreply.github.com>
2017-07-02 08:50:03 +0900

    Add unique

--- IZF.v
+++ IZF.v
@@ -11,3 +11,10 @@ Axiom EmptyAx : exists e, forall x, not (In x e).
 Axiom PairAx : forall a b, exists c, forall x, iff (In x c) (x = a \/ x = b).
 (* 和集合公理 *)
 Axiom UnionAx : forall a, exists b, forall x, iff (In x b) (exists u, In u a /\ In u x).
+
+(* ある述語を満たす集合が一つのみである *)
+Definition Unique (P : SET -> Prop) := (exists x, P x) /\ (forall x y, P x /\ P y -> x = y).
+(* Pを満たして一意に存在する集合 *)
+Axiom Uniqued : forall (P : SET -> Prop), Unique P -> SET.
+(* Uniquedの性質の公理 *)
+Axiom UniqueAx : forall (P : SET -> Prop) (U : Unique P), P (Uniqued P U).
