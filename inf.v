5a2efbbb660a6daf4d3f13a5e88dc3c5b094849c
Hexirp <Hexirp@users.noreply.github.com>
2017-07-23 17:49:05 +0900

    Add comp_r_prom

--- inf.v
+++ inf.v
@@ -1,4 +1,8 @@
-(* 無限集合に関する定理 *)
+(* 無限集合に関する定理
+ *
+ * 以下の文献を参考にした。
+ * http://konn-san.com/math/set-theory-seminar/2012-10-30.pdf
+ *)
 
 Load axioms.
 
@@ -8,27 +12,29 @@ Theorem comp_r_prom (P : SET -> Prop) : (exists a, comp_r P a) -> exists a, comp
 Proof.
  intros H.
  destruct H as [Ha HH].
- exists Ha.
+ exists (sep P Ha).
  unfold comp.
  intros x.
  split.
  -
-  assert (U := SepAx P Ha).
-  intros Q.
-  destruct U as [Ua UH].
-  unfold IsSep in UH.
-  unfold comp in UH.
-  assert (V := UH x).
-  destruct V as [Vl Vr].
+  intros H.
+  assert (U := SepUx P Ha x).
+  destruct U as [Ul Ur].
   apply proj1 with (In x Ha).
-  apply Vl.
+  apply Ul.
+  apply H.
  -
-  intros Q.
-  unfold comp_r in HH.
-  apply (HH x).
-  
- assert (U := SepAx P Ha).
- destruct U as [Ua UH].
- assert (Ux := UH x).
+  intros H.
+  assert (U := SepUx P Ha x).
+  destruct U as [Ul Ur].
+  apply Ur.
+  split.
+  +
+   apply H.
+  +
+   unfold comp_r in HH.
+   apply HH.
+   apply H.
+Qed.
 
 Definition IsNat (A : SET) := forall x, In x A <-> Natlike A x.
