c82c6b5719ac503798a27924e2126c09cc865afc
Hexirp <Hexirp@users.noreply.github.com>
2017-07-23 18:22:12 +0900

    Add UniqueNat

--- inf.v
+++ inf.v
@@ -37,4 +37,43 @@ Proof.
    apply H.
 Qed.
 
+Definition Natlike A x := x = empty \/ exists y, In y A /\ x = succ y.
+
+Definition IsInf' (A : SET) := forall x, Natlike A x -> In x A.
+
+Lemma inf_then_inf : forall A, IsInf A -> IsInf' A.
+Proof.
+ intros A H.
+ unfold IsInf'.
+ unfold Natlike.
+ intros x R.
+ destruct H as [Ho Hs].
+ destruct R as [Ro | Rs].
+ -
+  rewrite Ro.
+  apply Ho.
+ -
+  destruct Rs as [S Rs].
+  destruct Rs as [SA Rs].
+  rewrite Rs.
+  apply Hs.
+  apply SA.
+Qed.
+
+Lemma exists_inf' : exists x, IsInf' x.
+Proof.
+ destruct InfAx as [Inf Ax].
+ exists Inf.
+ apply inf_then_inf.
+ apply Ax.
+Qed.
+
 Definition IsNat (A : SET) := forall x, In x A <-> Natlike A x.
+
+Theorem UniqueNat : Unique IsNat.
+Proof.
+ split.
+ -
+  unfold IsNat.
+  destruct InfAx as [Inf Ax].
+  exists (sep
