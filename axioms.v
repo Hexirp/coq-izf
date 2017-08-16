0ece55cc873ddbfd90f2f2627e31be5307a24a56
Hexirp <Hexirp@users.noreply.github.com>
2017-08-16 23:12:15 +0900

    Update axioms.v

--- axioms.v
+++ axioms.v
@@ -3,19 +3,7 @@
  * https://plato.stanford.edu/entries/set-theory-constructive/axioms-CZF-IZF.html
  *)
 
-Require Import Init.
-
-(* 集合の型 *)
-Axiom SET : Type.
-(* 帰属関係の述語 *)
-Axiom In : SET -> SET -> Prop.
-(* 帰属関係の記法。∈の見立て。 *)
-Notation "x ':e' y" := (In x y) (at level 70).
-
-(* 包含関係 *)
-Definition Sub (A : SET) (B : SET) := forall x, x :e A -> x :e B.
-(* 包摂関係の記法。⊂の見立て。 *)
-Notation "x 'c=' y" := (Sub x y) (at level 70).
+Require Import Init set.
 
 (* ある述語を満たす集合が一つのみである *)
 Definition Unique (P : SET -> Prop) := (exists x, P x) /\ (forall x y, P x /\ P y -> x = y).
