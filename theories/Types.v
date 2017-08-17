deb1f5da8098ff16022a3caaf28f8df80458d460
Hexirp <Hexirp@users.noreply.github.com>
2017-08-17 15:46:57 +0900

    Add notations

--- theories/Types.v
+++ theories/Types.v
@@ -8,7 +8,12 @@ Axiom In : SET -> SET -> Prop.
 Notation "x ':e' y" := (In x y) (at level 70).
 (** 帰属関係の否定。∉の見立て。 *)
 Notation "x ':/e ' y" := (~ In x y) (at level 70).
- 
+
+(** 集合により限定された全称量化 *)
+Notation "'forall' x ':e' a ',' p" := (forall x : SET, x :e a -> p) (at level 190, x ident).
+(** 集合により限定された存在量化 *)
+Notation "'exists' x ':e' a ',' p" := (exists x : SET, x :e a /\ p) (at level 190, x ident).
+
 (* 包含関係 *)
 Definition Sub (A : SET) (B : SET) := forall x, x :e A -> x :e B.
 (* 包摂関係の記法。⊂の見立て。 *)
