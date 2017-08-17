9f273b5027accf928811c062b67709489646b14e
Hexirp <Hexirp@users.noreply.github.com>
2017-08-17 16:08:09 +0900

    Update notations

--- theories/Types.v
+++ theories/Types.v
@@ -10,11 +10,14 @@ Notation "x ':e' y" := (In x y) (at level 70).
 Notation "x ':/e ' y" := (~ In x y) (at level 70).
 
 (** 集合により限定された全称量化 *)
-Notation "'forall' x ':e' a ',' p" := (forall x : SET, x :e a -> p) (at level 190, x ident).
+Notation "'forall' x ':e' a ',' p"
+ := (forall x : SET, x :e a -> p) (at level 199, x ident, right associativity) : type_scope.
 (** 集合により限定された存在量化 *)
-Notation "'exists' x ':e' a ',' p" := (exists x : SET, x :e a /\ p) (at level 190, x ident).
+Notation "'exists' x ':e' a ',' p"
+ := (exists x : SET, x :e a /\ p) (at level 199, x ident, right associativity) : type_scope.
 
 (* 包含関係 *)
 Definition Sub (A : SET) (B : SET) := forall x, x :e A -> x :e B.
 (* 包摂関係の記法。⊂の見立て。 *)
-Notation "x 'c=' y" := (Sub x y) (at level 70).
+Notation "x 'c=' y" := (Sub x y) (at level 70) : type_scope.
+Notation "x 'c/=' y" := (~ Sub x y) (at level 70) : type_scope.
