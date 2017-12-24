15829cca2f1751ade0c4d18e514fc72d2b17d502
Hexirp <Hexirp@users.noreply.github.com>
2017-12-24 16:52:14 +0900

    Update

--- theories/Types.v
+++ theories/Types.v
@@ -9,13 +9,13 @@ Axiom elem : set -> set -> Prop.
 (** 帰属関係の記法。∈の見立て。 *)
 Notation "x ':e' y" := (elem x y) (at level 70) : type_scope.
 (** 帰属関係の否定。∉の見立て。 *)
-Notation "x ':/e ' y" := (~ x :e y) (at level 70) : type_scope.
+Notation "x ':/e' y" := (~ x :e y) (at level 70) : type_scope.
 (** 集合により限定された全称量化 *)
 Notation "'forall' x ':e' a ',' p"
-  := (forall x : set, x :e a -> p) (at level 199, x ident, right associativity) : type_scope.
+  := (forall x, x :e a -> p) (at level 199, x ident, right associativity) : type_scope.
 (** 集合により限定された存在量化 *)
 Notation "'exists' x ':e' a ',' p"
-  := (sigT set (fun x => x :e a * p)) (at level 199, x ident, right associativity) : type_scope.
+  := (exists x, x :e a /\ p) (at level 199, x ident, right associativity) : type_scope.
 
 (* 包含関係 *)
 Definition sub (a b : set) : Prop := forall x, x :e a -> x :e b.
@@ -44,3 +44,8 @@ Definition sub_exten (a b : set) : a c= b -> b c= a -> a ~ b := fun H I x => con
 Definition exten_sub_l (a b : set) : a ~ b -> a c= b := fun H x => proj1 (H x).
 (* 外延は包摂関係を導く（右） *)
 Definition exten_sub_r (a b : set) : a ~ b -> b c= a := fun H x => proj2 (H x).
+
+(* 弱い内包 *)
+Definition wcomp (P : set -> Prop) (a : set) := forall x, P x -> x :e a.
+Notation "x '<e' p" := (wcomp p x) (at level 70) : type_scope.
+Definition sub_wcomp (a b : set) : (a c= b) = (wcomp (fun x => x :e a) b) := eq_refl.
